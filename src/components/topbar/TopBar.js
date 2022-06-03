ed8d999fb2be3ebb644d5eb1692e6fd5d0102fa5
sherly <sharton1@asu.edu>
2022-06-03 10:42:52 +0800

    finish sidebar and single post

---
+++ src/components/topbar/TopBar.js
@@ -0,0 +1,38 @@
+import "./topbar.css"
+import shProfile from "../images/sh-circ.png"
+
+export default function TopBar() {
+    return (
+        <div className="top">
+            <div className="topLeft">
+                <i className="topSocialIcon fa-brands fa-github-square"></i>
+                <i className="topSocialIcon fa-brands fa-linkedin"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">
+                        HOME
+                    </li>
+                    <li className="topListItem">
+                        ABOUT
+                    </li>
+                    <li className="topListItem">
+                        CONTACT
+                    </li>
+                    <li className="topListItem">
+                        WRITE
+                    </li>
+                    <li className="topListItem">
+                        LOGOUT
+                    </li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img className="topProfileImg" src={shProfile} alt="profile" />
+                <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+
+            </div>
+
+        </div >
+    )
+}
