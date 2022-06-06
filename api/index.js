8d8e8aa2255e4e79093e9fd915c62cae0c41540b
sherly <sharton1@asu.edu>
2022-06-06 17:26:49 +0700

    added models folder

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-console.log("hello");
 
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then(console.log("connected to mongo"))
+    .catch(err => console.log("err"));
+
+app.use("/", (req, res) => {
+    console.log("this is main url");
+})
 app.listen("3000", () => {
     console.log("back end running");
 })
