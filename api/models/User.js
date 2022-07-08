fd7cd2a58aece94bc8a1c3bca1a24cca99bd96ed
sherly <sharton1@asu.edu>
2022-07-09 00:56:33 +0700

    edit user works

--- api/models/User.js
+++ api/models/User.js
@@ -3,37 +3,42 @@ const mongoose = require("mongoose");
 
 //- create interface / schema
 const UserSchema = new mongoose.Schema({
-    name: {
+    username: {
         type: String,
         required: [true, "cannot add an user without name"],
         unique: true
     },
-    profilePic: {
-        type: String,
-        default: ""
-    },
-
+    
     email: {
         type: String,
         required: [true, "cannot add an user without email"],
         unique: true
     },
-
+    
     about: {
         type: String,
         default: ""
     },
-
+    
     linkedin: {
         type: String,
         default: ""
     },
-
+    
     github: {
         type: String,
         default: ""
     },
-
+    
+    sub: {
+        type: String,
+        default: ""
+    },
+    profilePic: {
+        type: String,
+        default: ""
+    },
+    
 }, { timestamps: true })
 
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,79 +1,60 @@
 const router = require("express").Router();
 const User = require('../models/User');
-const Post = require('../models/Post');
-const bcrypt = require("bcrypt");
 
 
-// the CREATE is in auth.js
-// GET
-router.get("/:id",
+router.post("/",
     async (req, res) => {
-        // 1. check if the request comes from the same user in params
         try {
-            const user = await User.findById(req.params.id);
-            // only return the field without password
-            const { password, ...others } = user._doc;
-            res.status(200).json(others);
+
+            const userFound = await User.findOne({ sub: req.body.sub });
+
+            if(!userFound) {
+                // 1. create the object based on request
+                const userObj = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    about: req.body.about,
+                    linkedin: req.body.linkedin,
+                    github: req.body.github,
+                    sub: req.body.sub
+                });
+    
+                // 2. save new
+                const savedResponse = await userObj.save();
+                res.status(200).json(savedResponse);
+
+            } else {
+  
+                // 3. find by id and update
+                const updatedUser = await User.findByIdAndUpdate(userFound._id,
+                    {
+                        $set: req.body
+                    },
+                    { new: true });
+                res.status(200).json(updatedUser);
+            }
 
         } catch (err) {
-            res.status(404).json("User doesn't exists");
+            console.log(err);
         }
+
 
     })
 
-// UPDATE
-router.put("/:id",
+
+router.get("/:sub",
     async (req, res) => {
-        // compare the params and the user id in DB
-        if (req.params.id === req.body.userId) {
-            // 1. if user trying to change password
-            if (req.body.password) {
-                const salt = await bcrypt.genSalt(10);
-                req.body.password = await bcrypt.hash(req.body.password, salt);
-            }
+        try {
+           
+            // 1. find by sub
+            const user = await User.findOne({ sub: req.params.sub });
+            res.status(200).json(user);
 
-            try {
-                const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                    $set: req.body,
-                }, { new: true }) // return the updated user
+        } catch (err) {
+            console.log(err);
 
-                res.status(200).json(updatedUser);
-
-            } catch (err) {
-                res.status(500).json(err);
-            }
-        } else {
-            res.status(401).json("You can only update your account")
         }
     })
-
-
-// DELETE
-router.delete("/:id",
-    async (req, res) => {
-        // 1. check if the request comes from the same user in params
-        if (req.params.id === req.body.userId) {
-            try {
-                const user = await User.findById(req.params.id);
-
-                try {
-                    // 2. delete all post and the user
-                    await Post.deleteMany({ username: user.username });
-                    await User.findByIdAndDelete(req.params.id);
-                    res.status(200).json("User has been deleted");
-
-                } catch (err) {
-                    res.status(500).json(err);
-                }
-            } catch (err) {
-                res.status(404).json("User doesn't exists");
-            }
-
-        } else {
-            res.status(401).json("You can only update your account")
-        }
-    })
-
 
 
 
